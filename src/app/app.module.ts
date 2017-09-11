ac9d644659180f260cd99e3c471087f439a17058
JD Medina <jdmedina@gmail.com>
2017-09-11 15:57:58 -0700

    Adding previous work of WF MTC POC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { Manager } from '../pages/manager/manager';
 import { ListPage } from '../pages/list/list';
+import { DetailPage } from '../pages/detail/detail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    Manager,
+    ListPage,
+    DetailPage
   ],
   imports: [
     BrowserModule,
@@ -22,13 +26,52 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    Manager,
+    ListPage,
+    DetailPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Push,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(private push: Push) {
+    // check for permissions
+    this.push.hasPermission()
+    .then((res: any) => {
+      if (res.isEnabled) {
+        console.log('We have permission to send push notifications');
+      } else {
+        console.log('We do not have permission to send push notifications');
+      }
+    });
+
+    // to initialize push notifications
+    const options: PushOptions = {
+       android: {
+           senderID: '12345679'
+       },
+       ios: {
+           alert: 'true',
+           badge: true,
+           sound: 'true'
+       },
+       windows: {},
+       browser: {
+           pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+       }
+    };
+
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+
+    pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+
+    pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+  }
+}
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,36 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { DetailPage } from '../detail/detail';
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
   selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{
+    time: string,
+    sentBy: string,
+    customers: Array<{
+      ecn: string,
+      customerName: string,
+    }>,
+    comments: string
+  }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    this.items = [{
+      time: '08:01 am',
+      sentBy: 'Marlee Savory',
+      customers: [{
+        ecn: '1000-2000-3000',
+        customerName: 'Dr. Laurie A Johnson'
+        }, {
+          ecn: '1002-2002-3002',
+          customerName: 'Adam Johnson'
+        }],
+        comments: 'Customer wants to open an IRA for retirement. Expressed interest in other types of investement accounts.'
+      }, {
+      time: '08:57 am',
+      sentBy: 'Colton Greely',
+      customers: [{
+        ecn: '1001-2001-3001',
+        customerName: 'Donald Draper'
+      }],
+      comments: 'Customer wants to open an IRA for retirement'
+    }];
   }
 
   itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
+    this.navCtrl.push(DetailPage, {
       item: item
     });
   }
