9f2cb0dbc55a0396742cd81a71dae64f57b67357
JD Medina <jdmedina@gmail.com>
2017-10-18 15:21:21 -0700

    Commenting out Ionic push for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,38 +40,38 @@ import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
 export class AppModule {
   constructor(private push: Push) {
-    // check for permissions
-    this.push.hasPermission()
-    .then((res: any) => {
-      if (res.isEnabled) {
-        console.log('We have permission to send push notifications');
-      } else {
-        console.log('We do not have permission to send push notifications');
-      }
-    });
+    // // check for permissions
+    // this.push.hasPermission()
+    // .then((res: any) => {
+    //   if (res.isEnabled) {
+    //     console.log('We have permission to send push notifications');
+    //   } else {
+    //     console.log('We do not have permission to send push notifications');
+    //   }
+    // });
 
-    // to initialize push notifications
-    const options: PushOptions = {
-       android: {
-           senderID: '12345679'
-       },
-       ios: {
-           alert: 'true',
-           badge: true,
-           sound: 'true'
-       },
-       windows: {},
-       browser: {
-           pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-       }
-    };
+    // // to initialize push notifications
+    // const options: PushOptions = {
+    //    android: {
+    //        senderID: '12345679'
+    //    },
+    //    ios: {
+    //        alert: 'true',
+    //        badge: true,
+    //        sound: 'true'
+    //    },
+    //    windows: {},
+    //    browser: {
+    //        pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+    //    }
+    // };
 
-    const pushObject: PushObject = this.push.init(options);
+    // const pushObject: PushObject = this.push.init(options);
 
-    pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+    // pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
 
-    pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+    // pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
 
-    pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+    // pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
   }
 }
